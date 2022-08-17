1ba0cd358a9654048730cd2778b453028aefa511
Shalinda Fernando <it21801204@my.sliit.lk>
2022-08-17 20:33:42 +0530

    Exercise 3 completed

--- Tute03.c
+++ Tute03.c
@@ -14,6 +14,26 @@ sum = 1+2+3+...+10 = 55 */
 #include <stdio.h>
 int main() {
   
+  int counter, n, sum;
+  sum = 0;
+  
+  printf("Enter number of repetitions: ");
+  scanf("%d", &n);
+
+  for(counter = 1; counter <= n; counter++){
+
+    sum += counter;
+    
+    if(counter == n){
+      printf("%d = ", counter);
+    }else{
+      printf("%d + ", counter);
+    }
+    
+  }
+
+  printf("%d", sum);
+  
   return 0;
 }
 
