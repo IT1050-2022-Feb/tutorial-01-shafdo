8505ce842e37f4f4339228b3f0cdde1bd57c8c47
Shalinda Fernando <it21801204@my.sliit.lk>
2022-08-17 17:47:43 +0530

    Exercise 1 completed

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,18 @@
 
 int main() {
   
+  int mark1, mark2;
+  float average;
+  
+  printf("Enter mark 1 : ");
+  scanf("%d", &mark1);
+  printf("Enter mark 2 : ");
+  scanf("%d", &mark2);
+  
+  average = (mark1 + mark2) / 2;
+  
+  printf("Your average is = %.2f\n", average);
+   
   return 0;
 }
 
